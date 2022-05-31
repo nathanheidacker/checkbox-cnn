b52b28ab662de0cf7cce00a1c87d7b465aed3de6
Nathan Heidacker <nathanheidacker@gmail.com>
2022-05-31 18:48:08 +0900

    Initial training pipeline complete

--- src/train.py
+++ src/train.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Helper functions for training and evaluating the model. Trains the model when
-run as __main__.
+Helper functions for training the model. Trains the model when run as __main__.
 """
 
 # Standard Imports
 from __future__ import annotations
 
 # Third Party Imports
+import tqdm
 import torch
+from torch.utils.data import DataLoader
 
 # Local Imports
 from .data import CheckboxData
@@ -18,17 +19,29 @@ from .model import CheckboxCNN
 from typing import Any
 
 
-def train(epochs: int = 20):
-    return
+def train(
+    model: torch.nn.Module,
+    data: DataLoader,
+    epochs: int = 20,
+    criterion=None,
+    optimizer=None,
+) -> dict[str, torch.Tensor]:
+    criterion = torch.nn.CrossEntropyLoss if criterion is None else criterion
+    optimizer = torch.optim.Adam(model.parameters)
+    for epoch in tqdm(range(epochs)):
+        for i, (features, labels) in enumerate(data):
+            optimizer.zero_grad()
+            predicted = model(features)
+            loss = criterion(predicted, labels)
+            loss.backward()
+            optimizer.step()
 
-
-def test():
-    return
-
-
-def evaluate():
-    return
+    return model.state_dict()
 
 
 if __name__ == "__main__":
-    train()
+    model = CheckboxCNN()
+    data = CheckboxData()
+    train, test = data.load()
+    weights = train(model, train)
+    torch.save(weights, data.path.parent)
