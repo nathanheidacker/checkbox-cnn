688f9a705cb2b288d2ceb891b5f63e12d78b106f
Nathan Heidacker <nathanheidacker@gmail.com>
2022-06-01 12:17:45 +0900

    Added evaluate function

--- src/evaluate.py
+++ src/evaluate.py
@@ -14,14 +14,35 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 
 # Local Imports
-from .data import CheckboxData
+from data import CheckboxData
+from model import CheckboxCNN
 
 # Typing
-from typing import Callable
+from typing import (
+    Callable,
+    Optional,
+)
 
 
-def evaluate(model: nn.Module, data: DataLoader, criterion: Callable = None) -> str:
-    pass
+def evaluate(
+    model: nn.Module, data: DataLoader, criterion: Optional[Callable] = None
+) -> str:
+    model.eval()
+    loss = 0
+    correct = 0
+
+    criterion = nn.CrossEntropyLoss() if criterion is None else criterion
+
+    for features, labels in data:
+        preds = model(features)
+        loss += criterion(preds, labels)
+        preds = preds.argmax(dim=1)
+        correct += (preds == labels).sum()
+
+    accuracy = correct / len(data.dataset)
+    loss = loss / len(data.dataset)
+
+    return loss.item(), accuracy.item()
 
 
 def visualize():
@@ -31,3 +52,6 @@ def visualize():
 if __name__ == "__main__":
     data = CheckboxData()
     train, test = data.load()
+    model = CheckboxCNN("weights.bin")
+    loss, accuracy = evaluate(model, test)
+    print(loss, accuracy)
