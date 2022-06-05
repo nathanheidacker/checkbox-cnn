76b6a43573f836754e8cdac8151cd2e1847a4da8
Nathan Heidacker <nathanheidacker@gmail.com>
2022-06-06 07:57:49 +0900

    added windows specific commands

--- run.sh
+++ run.sh
@@ -3,4 +3,7 @@ printf "The following commands are available to you:\n"
 printf "    For training:      python src/train.py <\"v1\" or \"v2\">\n"
 printf "    For inference:     python src/evaluate.py <your_image_path or 'all'>\n"
 printf "    For data stats:    python src/data.py <n training samples>\n"
-docker run -it --rm cnn bash
+if [["$OSTYPE" == "msys"]]: then
+    winpty docker run -it --rm cnn bash
+else
+    docker run -it --rm cnn bash
